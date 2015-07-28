ae317b7b1bdcc07140034093098cc535d0d3584b
Trevor Salom <trvrsalom@gmail.com>
2015-07-27 21:08:26 -0700

    Added cs autorun/compile. Uses mono/mcs

---
+++ cscomp.sh
@@ -0,0 +1,20 @@
+RED='\033[0;31m'
+GRE='\033[0;32m';
+NC='\033[0m'
+if [ ! "$1" == "" ]; then
+  echo "Compiling..."
+  RESP= $(mcs $1.cs)
+  if [[ "$RESP" == "" ]]; then
+      printf "${GRE}Success!${NC}\nRunning Program:\n"
+      echo "======================"
+      echo $(mono $1.exe)
+      find . -name '*.exe' -delete
+  else
+      printf "${RED}Looks like you have errors...${NC}\n"
+      echo "======================"
+      echo "$RESP";
+
+  fi
+else
+  printf "${RED}Please provide an existing java file without the .cs extension. (./cscomp.sh yourcsfile)${NC}\n"
+fi
